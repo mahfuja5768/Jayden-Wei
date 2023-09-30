385c0c560d9f5bee1c1c97b489791b37a35c5a13
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-09-30 11:12:52 +0600

    add active class

--- js/main.js
+++ js/main.js
@@ -1,26 +1,3 @@
-// /* show-menu */
-// const showMenu = (toggleId, navId) => {
-//   const toggle = document.getElementById(toggleId),
-//     nav = document.getElementById(navId);
-//   hum = document.getElementsByClassName("hamburger");
-//   if (toggle && nav && hum) {
-//     toggle.addEventListener("click", () => {
-//       nav.classList.toggle("show-menu");
-//       hum.classList.add("open");
-//     });
-//   }
-// };
-// showMenu("nav_toggle", "nav_menu");
-
-// /* remove-menu */
-// const navLink = document.querySelectorAll(".nav-link");
-
-// function linkAction() {
-//   const navMenu = document.getElementById("nav_menu");
-//   navMenu.classList.remove("show-menu");
-// }
-// navLink.forEach((n) => n.addEventListener("click", linkAction));
-
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
@@ -35,11 +12,11 @@ const navLink = document.querySelectorAll(".nav-link").forEach((n) => n.addEvent
   navMenu.classList.remove("open");
 }));
 
-const activePage = window.location.pathname;
-// console.log(activePage)
-const navLinks = document.querySelectorAll(".nav-list a").forEach((link) => {
-  // console.log(link.href)
-  if (link.href.includes(`${activePage}`)) {
-    link.classList.add("active");
-  }
-});
+// const activePage = window.location.pathname;
+// // console.log(activePage)
+// const navLinks = document.querySelectorAll(".nav-list a").forEach((link) => {
+//   // console.log(link.href)
+//   if (link.href.includes(`${activePage}`)) {
+//     link.classList.add("active");
+//   }
+// });
